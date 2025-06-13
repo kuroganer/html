18125e89c2b2fcbd989ecb28c5f3b137e9b90ff1
Robson <tobikyoshiro@gmail.com>
2025-06-13 12:01:06 -0300

    Delete Projeto directory

--- Projeto/control/listarUsuarioControl.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once '../model/DAO/UsuarioDAO.php';
-    require_once '../model/DTO/UsuarioDTO.php';
-
-    $usuarioDAO =  new UsuarioDAO();
-  $todosUsuarios = $usuarioDAO->listarUsuarios();
-?>
